63ac538af4c6b9774996b1e684567c85d74234d8
Noname34169 <salomovasil0@gmail.com>
2024-11-19 16:57:11 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.scss'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Contacts from './pages/Contacts'
+import Blog from './pages/Blog'
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <>
+
+    <Header />
+    
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/blog' element={<Blog />} />
+      <Route path='/contacts' element={<Contacts />} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Brands from '../components/Brands/Brands'
+
+const Home = () => {
+  return (
+    <>
+    <Hero />
+    <Brands />
+    </>
+  )
+}
+
+export default Home
